95ed85d13397a11b300711cd7daf337339141732
ML Zhang <814548121@qq.com>
2024-01-13 19:27:42 +0800

    试验

---
+++ 数据/weather-icon (1).js
@@ -0,0 +1,34 @@
+const weather_icon = [
+      {
+            type: "0-天晴",
+            url: "https://gw.alipayobjects.com/mdn/antv_site/afts/img/A*kzTMQqS2QdUAAAAAAAAAAABkARQnAQ"
+      }, {
+            type: "1-多云",
+            url: "https://gw.alipayobjects.com/mdn/antv_site/afts/img/A*jH1XRb7F7hMAAAAAAAAAAABkARQnAQ"
+      },
+      {
+            type: "2-小雨",
+            url: "https://gw.alipayobjects.com/mdn/antv_site/afts/img/A*DccRTI6ZRLoAAAAAAAAAAABkARQnAQ"
+      },
+      {
+            type: "3-中雨",
+            url: "https://gw.alipayobjects.com/mdn/antv_site/afts/img/A*f-wyS7ad5p0AAAAAAAAAAABkARQnAQ"
+      },
+      {
+            type: "4-大雨",
+            url: "https://gw.alipayobjects.com/mdn/antv_site/afts/img/A*YaKSTr3L5i8AAAAAAAAAAABkARQnAQ"
+      }
+      ,
+      {
+            type: "5-雷阵雨",
+            url: "https://gw.alipayobjects.com/mdn/antv_site/afts/img/A*LyuVRowl6nAAAAAAAAAAAABkARQnAQ"
+      },
+      {
+            type: "6-中雪",
+            url: "https://gw.alipayobjects.com/mdn/antv_site/afts/img/A*iQKoS6I-rO8AAAAAAAAAAABkARQnAQ"
+      },
+      {
+            type: "7-大雪",
+            url: "https://gw.alipayobjects.com/mdn/antv_site/afts/img/A*9Q0QS4GdaYcAAAAAAAAAAABkARQnAQ"
+      }
+]
